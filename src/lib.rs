f9c258566a3a40dacba96b856e5be3ea627bd5df
aristotle9 <lanfan.1987@gmail.com>
2015-11-22 01:44:43 +0800

    rgb image output

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,13 @@ pub struct RenderOption {
     pub padding: i32,
 }
 
+impl RenderOption {
+    fn get_blank_bitmap(&self) -> Bitmap {
+        let a = self.size + self.padding;
+        Bitmap::new(a, a, a)
+    }
+}
+
 pub struct CharImageRender {
     pub ft_lib: FT_Library,
     pub ft_face: FT_Face,
@@ -24,6 +31,20 @@ pub struct Bitmap {
     pub h: i32,
     pub pitch: i32,
     pub buffer: Vec<u8>,
+}
+
+pub fn get_bg_color(c: char) -> u32 {
+    match (c as u32) % 9 {
+        0 => 0x45BDF3,
+		1 => 0xE08F70,
+		2 => 0x4DB6AC,
+		3 => 0x9575CD,
+		4 => 0xB0855E,
+		5 => 0xF06292,
+		6 => 0xA3D36C,
+		7 => 0x7986CB,
+		_ => 0xF1B91D,
+    }
 }
 
 impl Bitmap {
@@ -57,11 +78,32 @@ impl Bitmap {
         }
     }
 
-    pub fn to_png_image(self) -> Image {
+    pub fn to_gray_png_image(self) -> Image {
         Image {
             width: self.w as u32,
             height: self.h as u32,
             pixels: PixelsByColorType::K8(self.buffer),
+        }
+    }
+
+    pub fn to_rgb_png_image(self, bg_color: u32) -> Image {
+        let r = (bg_color >> 16) & 0xff;
+        let g = (bg_color >>  8) & 0xff;
+        let b = bg_color & 0xff;
+        let pixels: Vec<u8> = self.buffer.into_iter().flat_map(|c| {
+            if c == 0 {
+                vec![r as u8, g as u8, b as u8].into_iter()
+            } else {
+                let fac = (255f64 - c as f64) / 255f64;
+                vec![(c as f64 + (r as f64 * fac)) as u8,
+                     (c as f64 + (g as f64 * fac)) as u8,
+                     (c as f64 + (b as f64 * fac)) as u8].into_iter()
+            }
+        }).collect();
+        Image {
+            width: self.w as u32,
+            height: self.h as u32,
+            pixels: PixelsByColorType::RGB8(pixels)
         }
     }
 
@@ -127,7 +169,7 @@ impl CharImageRender {
     }
 
     pub fn render(&mut self, ch: char, opt: &RenderOption) -> Bitmap {
-        let mut img = Bitmap::new(opt.size, opt.size, opt.size);
+        let mut img = opt.get_blank_bitmap();
         unsafe {
             FT_Set_Pixel_Sizes(self.ft_face, 0, opt.size as u32);
             FT_Load_Char(self.ft_face, ch as u64, FT_LOAD_DEFAULT);
@@ -136,7 +178,7 @@ impl CharImageRender {
             FT_Render_Glyph(slot, FT_RENDER_MODE_NORMAL);
 
             let bitmap = Bitmap::from_ft_bitmap(&(*slot).bitmap);
-            Bitmap::bitmap_blit(&bitmap, (opt.size - bitmap.w) / 2, (opt.size - bitmap.h) / 2, &mut img);
+            Bitmap::bitmap_blit(&bitmap, (img.w - bitmap.w) / 2, (img.h - bitmap.h) / 2, &mut img);
         }
         img
     }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ extern crate png;
 extern crate freetype;
 
 mod lib;
-
 use lib::*;
 
 use png::{to_vec, store_png};
 
 fn main() {
     let mut render = CharImageRender::new();
-    for c in "DEFabc你好".chars() {
-        let bitmap = render.render(c, &RenderOption { size: 36, padding: 0 });
-        println!("{}", bitmap);
-        let mut image = bitmap.to_png_image();
-        // store_png(&mut image, format!("outputs/img-{}.png", c)).unwrap();
+    for c in "abcDEF赵钱孙李周吴郑王".chars() {
+        let bitmap = render.render(c, &RenderOption { size: 72, padding: 50});
+        // println!("{}", bitmap);
+        let mut image = bitmap.to_rgb_png_image(get_bg_color(c));
+        // let mut image = bitmap.to_gray_png_image();
+        // store_png(&mut image, format!("outputs/img-{:04x}.png", c as u32)).unwrap();
+        println!("{} done", c);
     }
 }
