14184529bb6477e94b128ceb37f17cc0d5483437
aristotle9 <lanfan.1987@gmail.com>
2015-11-23 01:44:50 +0800

    add http server

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 extern crate png;
 extern crate freetype;
+extern crate hyper;
+extern crate url;
 
 mod lib;
 use lib::*;
 
+use hyper::Server;
+use hyper::server::{Request, Response};
 use png::{to_vec, store_png};
 
-fn main() {
+const RENDER_OPTION: RenderOption = RenderOption { size: 72, padding: 50};
+
+fn png_image_from_char(c: char) -> Vec<u8> {
     let mut render = CharImageRender::new();
-    for c in "abcDEF赵钱孙李周吴郑王".chars() {
-        let bitmap = render.render(c, &RenderOption { size: 72, padding: 50});
-        // println!("{}", bitmap);
-        let mut image = bitmap.to_rgb_png_image(get_bg_color(c));
-        // let mut image = bitmap.to_gray_png_image();
-        // store_png(&mut image, format!("outputs/img-{:04x}.png", c as u32)).unwrap();
-        println!("{} done", c);
-    }
+    let bmp = render.render(c, &RENDER_OPTION);
+    let mut img = bmp.to_rgb_png_image(get_bg_color(c));
+    to_vec(&mut img).expect("encoding png err")
+}
+
+fn main() {
+    Server::http("127.0.0.1:3000").expect("start server failed!").handle(|req: Request, mut res: Response| {
+        if req.method == hyper::method::Method::Get {
+            let uri = match req.uri {
+                hyper::uri::RequestUri::AbsolutePath(uri) => url::percent_encoding::lossy_utf8_percent_decode(uri.as_bytes()),
+                _ => String::new()
+            };
+            let c = uri.chars().nth(1).unwrap_or('X');
+            let img = png_image_from_char(c);
+            res.headers_mut().set_raw("content-type", vec![b"image/png".to_vec()]);
+            res.send(&img).expect("send image failed");
+        } else {
+            *res.status_mut() = hyper::status::StatusCode::NotFound;
+        }
+    }).expect("add handle failed");
 }
